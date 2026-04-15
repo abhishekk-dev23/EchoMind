a095d97eabe979e4481e59b4d79dc665d4adfee3
Abhishek kumar <abishek2002tk@gmail.com>
2026-04-15 11:26:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://echomind-frontend-3jmp.onrender.com/"
+    "https://echomind-frontend-3jmp.onrender.com"
 ];
 
 app.use(
